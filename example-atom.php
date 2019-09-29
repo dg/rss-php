72ab91ee83668f64a170406896409f9213a4b4d9
David Grudl <david@grudl.com>
2019-09-29 10:53:44 +0200

    coding style

--- example-atom.php
+++ example-atom.php
@@ -12,15 +12,15 @@ $atom = Feed::loadAtom('https://php.vrana.cz/atom.php');
 
 ?>
 
-<h1><?php echo htmlSpecialChars($atom->title) ?></h1>
+<h1><?php echo htmlspecialchars($atom->title) ?></h1>
 
 <?php foreach ($atom->entry as $entry): ?>
-	<h2><a href="<?php echo htmlSpecialChars($entry->link['href']) ?>"><?php echo htmlSpecialChars($entry->title) ?></a>
-	<small><?php echo date("j.n.Y H:i", (int) $entry->timestamp) ?></small></h2>
+	<h2><a href="<?php echo htmlspecialchars($entry->link['href']) ?>"><?php echo htmlspecialchars($entry->title) ?></a>
+	<small><?php echo date('j.n.Y H:i', (int) $entry->timestamp) ?></small></h2>
 
 	<?php if ($entry->content['type'] == 'html'): ?>
 		<div><?php echo $entry->content ?></div>
 	<?php else: ?>
-		<p><?php echo htmlSpecialChars($entry->content) ?></p>
+		<p><?php echo htmlspecialchars($entry->content) ?></p>
 	<?php endif ?>
 <?php endforeach ?>
--- example-rss.php
+++ example-rss.php
@@ -11,17 +11,17 @@ $rss = Feed::loadRss('https://phpfashion.com/feed/rss');
 
 ?>
 
-<h1><?php echo htmlSpecialChars($rss->title) ?></h1>
+<h1><?php echo htmlspecialchars($rss->title) ?></h1>
 
-<p><i><?php echo htmlSpecialChars($rss->description) ?></i></p>
+<p><i><?php echo htmlspecialchars($rss->description) ?></i></p>
 
 <?php foreach ($rss->item as $item): ?>
-	<h2><a href="<?php echo htmlSpecialChars($item->link) ?>"><?php echo htmlSpecialChars($item->title) ?></a>
-	<small><?php echo date("j.n.Y H:i", (int) $item->timestamp) ?></small></h2>
+	<h2><a href="<?php echo htmlspecialchars($item->link) ?>"><?php echo htmlspecialchars($item->title) ?></a>
+	<small><?php echo date('j.n.Y H:i', (int) $item->timestamp) ?></small></h2>
 
 	<?php if (isset($item->{'content:encoded'})): ?>
 		<div><?php echo $item->{'content:encoded'} ?></div>
 	<?php else: ?>
-		<p><?php echo htmlSpecialChars($item->description) ?></p>
+		<p><?php echo htmlspecialchars($item->description) ?></p>
 	<?php endif ?>
 <?php endforeach ?>
